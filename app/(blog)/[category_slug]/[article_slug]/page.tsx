73005cb6219ae07596a0682ddaca5fb24f3c8226
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-04-01 12:30:46 +0900

    articleとarticlesを取得する関数を作成

--- app/(blog)/[category_slug]/[article_slug]/page.tsx
+++ app/(blog)/[category_slug]/[article_slug]/page.tsx
@@ -1,7 +1,12 @@
-const page = () => {
-    return (
-      <div>page</div>
-    )
-  }
-  
-  export default page
+import { getArticle, getArticles } from "@/app/component/lib/ArticleServise";
+
+const page = async ({ params }: { params: { article_slug: string } }) => {
+  const article = await getArticle(params.article_slug);
+  const articles = await getArticles();
+  console.log("article:", article);
+  console.log("articles:", articles);
+
+  return <div></div>;
+};
+
+export default page;
