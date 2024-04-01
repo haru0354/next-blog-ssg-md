dd1b88f8109d139b4de5c67dd7c48c3c7783dd3b
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-04-01 14:09:48 +0900

    サイドメニューの作成

---
+++ app/component/SideMenu.tsx
@@ -0,0 +1,13 @@
+import SideCategory from "./sideMenu/SideCategory"
+import SideNewArticle from "./sideMenu/SideNewArticle"
+
+const SideMenu = () => {
+  return (
+    <div>
+        <SideCategory/>
+        <SideNewArticle/>
+    </div>
+  )
+}
+
+export default SideMenu
