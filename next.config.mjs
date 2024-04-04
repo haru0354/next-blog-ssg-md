561ed92aefff5e14ae2c66335b53436d6868095f
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-04-04 13:24:20 +0900

    build時に画像のリサイズとwebpへの変換が行われるファイルの作成と設定の追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    output: 'export',
-  };
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
 
 
 export default nextConfig;
