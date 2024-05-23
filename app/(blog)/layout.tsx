8f13c5e8a5eeb6e6e0520b1b75770399db2e4e4c
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-05-23 22:23:15 +0900

    headerの中にグローバルメニューを移動

--- app/(blog)/layout.tsx
+++ app/(blog)/layout.tsx
@@ -1,7 +1,6 @@
 import Footer from "@/app/component/Footer";
 import Header from "@/app/component/Header";
 import SideMenu from "@/app/component/SideMenu";
-import GlobalMenu from "../component/GlobalMenu";
 
 export default function RootLayout({
   children,
@@ -11,7 +10,6 @@ export default function RootLayout({
   return (
     <>
       <Header />
-      <GlobalMenu />
       <main className="flex justify-center mb-20">
         <div className="max-w-[1150px] flex flex-wrap justify-center">
           <div className="flex flex-col flex-wrap w-full md:w-[800px] md:mr-6">
